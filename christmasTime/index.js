ed72ff0da1da7764fc266fe6902415c2a2809788
LukeBrazil <Luke.Brazil13@icloud.com>
2021-02-11 10:37:37 -0600

    more algos

---
+++ christmasTime/index.js
@@ -0,0 +1,9 @@
+function timeForMilkAndCookies(date) {
+	let month = date.getMonth()
+	let day = date.getDate()
+  if(month == 11 && day == 24) {
+    return true
+  } else {
+    return false
+  }
+}
---
+++ salesByMatch/index.js
@@ -0,0 +1,9 @@
+ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+
+function findMatches(ar) {
+    let matches = 0
+    ar.sort((a, b) => a - b)
+    console.log(ar)
+}
+
+findMatches(ar)
